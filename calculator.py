0783ee823a484cd229946c1d7e8709911dfe29a9
marcelinne <marcela.montalvo@epn.edu.ec>
2023-11-03 20:28:37 -0500

    feat(calculator.kv) Create the struccture for the calculator with the basic operations

--- calculator.py
+++ calculator.py
@@ -12,18 +12,63 @@ Window.size = (500, 700)
 Builder.load_file('calculator.kv')
 #Construct the Layout
 class MyLayout(Widget):
-    def press(self):
-        #Create variables for our widget
-        name=self.ids.name_input.text #Text Input
-        #Update the label
-        self.ids.name_label.text = f'Hello {name}!'
-        #Clear input box
-        self.ids.name_input.text = ''
+    def clear(self):
+        self.ids.calc_input.text = '0'
+    #Create a button pressing function
+    def button_press(self, button):
+        #Create a variable that contains whatever is in the input field
+        prior = self.ids.calc_input.text 
+        #Determine if 0 is in the input field
+        if prior == '0':
+            self.ids.calc_input.text = ''
+            self.ids.calc_input.text = f'{button}'
+        else:
+            self.ids.calc_input.text = f'{prior}{button}'
+        #Test for error first
+        if "Error" in prior:
+            prior = ''
+
+        #Determine if 0 is in the input field
+        if prior == '0':
+            self.ids.calc_input.text = ''
+            self.ids.calc_input.text = f'{button}'
+        else:
+            self.ids.calc_input.text = f'{prior}{button}'
+    
+    #Create addition function
+    def add(self):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}+'
+    #Create subtraction function
+    def subtract(self):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}-'
+    #Create multiplication function
+    def multiply(self):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}*'
+    #Create division function
+    def divide(self):
+        prior = self.ids.calc_input.text
+        self.ids.calc_input.text = f'{prior}/'
+    #Create equals function
+    def equals(self):
+        prior = self.ids.calc_input.text
+        #Addition 
+        if "+" in prior:
+            num_list= prior.split("+")
+            answer = 0
+            #Loop through our list
+            for number in num_list:
+                answer = answer + int(number)
+            #print the answer in the text box
+            self.ids.calc_input.text = str(answer)
+
 
 #Call the class
 class CalculatorApp(App):
     def build(self):
-       #Window.clearcolor = (1, 1, 1, 1)
+    #Window.clearcolor = (1, 1, 1, 1)
         return MyLayout()
 
 if __name__ == '__main__':
